0f006d0e3a4d40ff7233cedeac23d77dfea20222
Yauheniya Viarbitskaya <evgeniya.verbiczkaya98@gmail.com>
2018-09-24 17:18:03 +0300

    z

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ module.exports = function solveEquation(equation) {
     equation = equation.split('^2');
     let b = +equation[1];
     let solutions = [];
-    let d = b * b - 4 * a * c;
-    let x1 = (-b + Math.sqrt(d)) / (2 * a);
-    let x2 = (-b - Math.sqrt(d)) / (2 * a);
-    if (x1 < x2 && x2 > 0) {
-        solutions = [Math.round(x1), Math.round(x2)];
-    } else if (x1 < x2) {
-        solutions = [Math.round(x1), Math.round(x2)];
-    } else {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    (x1 < x2 && x2 > 0) ? solutions = [Math.round(x1), Math.round(x2)]:
+        (x1 < x2) ? solutions = [Math.round(x1), Math.round(x2)] :
         solutions = [Math.round(x2), Math.round(x1)];
-    }
     return solutions;
 }
